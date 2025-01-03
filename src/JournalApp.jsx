c8ea41a2d06f4449f94f299fdb48e1c8ab0537dc
EzeBaeza <ezeee.baeza@gmail.com>
2025-01-03 15:57:49 -0300

    commit

---
+++ src/JournalApp.jsx
@@ -0,0 +1,11 @@
+import { AppRouter } from "./router/AppRouter"
+import { AppTheme } from "./theme/AppTHeme"
+
+
+export const JournalApp = () => {
+  return (
+    <AppTheme>
+        <AppRouter/>
+    </AppTheme>
+  )
+}
